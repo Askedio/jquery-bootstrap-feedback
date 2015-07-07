58a56cbb4d46130631861de704d9516f3c88001d
william bowman <gcphost@gmail.com>
2015-07-07 01:07:16 -0500

    More test ing

--- report.php
+++ report.php
@@ -1,3 +1,4 @@
 <?php
 
+sleep(2);
 echo json_encode(array('result' => 'success'));
--- resources/js/site.js
+++ resources/js/site.js
@@ -4,12 +4,11 @@
 		$(this).find('.dropdown-menu-form').on('click', function(e){e.stopPropagation()})
 
 		$(this).find('.screenshot').on('click', function(){
-			$this=$(this);
-			$this.find('i').removeClass('fa-camera fa-check').addClass('fa-refresh fa-spin');
+			$(this).find('i').removeClass('fa-camera fa-check').addClass('fa-refresh fa-spin');
 			html2canvas($(document.body), {
 				onrendered: function(canvas) {
-					$this.find('.screen-uri').val(canvas.toDataURL("image/png"));
-					$this.find('i').removeClass('fa-refresh fa-spin').addClass('fa-check');
+					self.find('.screen-uri').val(canvas.toDataURL("image/png"));
+					self.find('.cam').removeClass('fa-refresh fa-spin').addClass('fa-check');
 				}
 			});
 		});
@@ -18,20 +17,23 @@
 			self.find('.dropdown-toggle').dropdown('toggle');
 			self.find('.reported, .failed').hide();
 			self.find('.report').show();
-			self.find('i').removeClass('fa-check').addClass('fa-camera');
+			self.find('.cam').removeClass('fa-check').addClass('fa-camera');
 		    self.find('.screen-uri').val('');
+		    self.find('textarea').val('');
 		});
 
 		failed = function(){
-			self.find('.report').hide();
+			self.find('.loading').hide();
 			self.find('.failed').show();
 			if(fail) fail();
 		}
 
 		$(this).find('form').on('submit', function(){
+			self.find('.report').hide();
+			self.find('.loading').show();
 			$.post( $(this).attr('action'), $(this).serialize(), null, 'json').done(function(res){
 				if(res.result == 'success'){
-					self.find('.report').hide();
+					self.find('.loading').hide();
 					self.find('.reported').show();
 					if(success) success();
 				} else failed();
