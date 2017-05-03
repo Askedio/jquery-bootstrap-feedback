f41b5f852dc6616aba2ce5f359119df8c0e836d5
Kaspars <kasparsj@gmail.com>
2017-05-03 22:24:34 +0300

    reset bug fix

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,53 +1,56 @@
 (function ( $ ) {
     $.fn.feedback = function(success, fail) {
-    	var self = $(this);
-		self.find('.dropdown-menu-form').on('click', function(e){e.stopPropagation()});
+    	$(this).each(function() {
+            var self = $(this),
+                form = self.find('form');
 
-		self.find('.feedback-screenshot').on('click', function(){
-			self.find('.feedback-cam').removeClass('fa-camera fa-check').addClass('fa-refresh fa-spin');
-			if (self.is(".modal")) {
-				$(".modal-backdrop").attr("data-html2canvas-ignore", "true");
-			}
-			html2canvas($(document.body), {
-				onrendered: function(canvas) {
-					self.find('.screen-uri').val(canvas.toDataURL("image/png"));
-					self.find('.feedback-cam').removeClass('fa-refresh fa-spin').addClass('fa-check');
-				}
-			});
-		});
+            self.find('.dropdown-menu-form').on('click', function(e){e.stopPropagation()});
 
-		self.find('.feedback-close').on('click', function(){
-            self.find('.dropdown-toggle').dropdown('toggle');
-			reset();
-		});
-		
-		function reset() {
-            self.find('.feedback-reported, .feedback-failed').hide();
-            self.find('.feedback-report').show();
-            self.find('.feedback-cam').removeClass('fa-check').addClass('fa-camera');
-            self.find('.feedback-screen-uri').val('');
-            self.find('textarea').val('');
-		}
+            self.find('.feedback-screenshot').on('click', function(){
+                self.find('.feedback-cam').removeClass('fa-camera fa-check').addClass('fa-refresh fa-spin');
+                if (self.is(".modal")) {
+                    $(".modal-backdrop").attr("data-html2canvas-ignore", "true");
+                }
+                html2canvas($(document.body), {
+                    onrendered: function(canvas) {
+                        self.find('.screen-uri').val(canvas.toDataURL("image/png"));
+                        self.find('.feedback-cam').removeClass('fa-refresh fa-spin').addClass('fa-check');
+                    }
+                });
+            });
 
-		function failed(){
-			self.find('.feedback-loading').hide();
-			self.find('.feedback-failed').show();
-			if(fail) fail();
-		}
+            self.find('.feedback-close').on('click', function(){
+                self.find('.dropdown-toggle').dropdown('toggle');
+                reset();
+            });
 
-		self.find('form').on('submit', function(){
-			self.find('.feedback-report').hide();
-			self.find('.feedback-loading').show();
-			$.post( $(this).attr('action'), $(this).serialize(), null, 'json').done(function(res){
-				if(res.result === 'success'){
-					self.find('.feedback-loading').hide();
-					self.find('.feedback-reported').show();
-					if(success) success();
-				} else failed();
-			}).fail(function(){
-				failed();
-			});
-			return false;
+            function reset() {
+                self.find('.feedback-reported, .feedback-failed').hide();
+                self.find('.feedback-report').show();
+                self.find('.feedback-cam').removeClass('fa-check').addClass('fa-camera');
+                form[0].reset();
+            }
+
+            function failed(){
+                self.find('.feedback-loading').hide();
+                self.find('.feedback-failed').show();
+                if(fail) fail();
+            }
+
+            form.on('submit', function(){
+                self.find('.feedback-report').hide();
+                self.find('.feedback-loading').show();
+                $.post( $(this).attr('action'), $(this).serialize(), null, 'json').done(function(res){
+                    if(res.result === 'success'){
+                        self.find('.feedback-loading').hide();
+                        self.find('.feedback-reported').show();
+                        if(success) success();
+                    } else failed();
+                }).fail(function(){
+                    failed();
+                });
+                return false;
+            });
 		});
 	};
 }( jQuery ));
